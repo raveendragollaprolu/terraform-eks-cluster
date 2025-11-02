882ade9adbe4d4ddab92c7ac78600717d40f2055
raveendragollaprolu <91277667+raveendragollaprolu@users.noreply.github.com>
2025-11-02 16:50:22 +0530

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,7 +1,7 @@
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "20.8.4"
+  version         = "20.8.5"
   cluster_name    = local.cluster_name
   cluster_version = var.kubernetes_version
   subnet_ids      = module.vpc.private_subnets
